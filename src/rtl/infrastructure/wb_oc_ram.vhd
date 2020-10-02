deb0b2f3dbc01466417c1f67b71601e099b433d7
veeYceeY <veeyceey@gmail.com>
2020-10-02 11:28:04 +0530

    Updated ram to support byte , half word access

--- src/rtl/infrastructure/wb_oc_ram.vhd
+++ src/rtl/infrastructure/wb_oc_ram.vhd
@@ -24,8 +24,12 @@ signal mem : mem_type;
 attribute rom_style of mem : signal is "block";
 signal data : std_logic_vector(31 downto 0);
 signal addr : std_logic_vector(13 downto 0);
+signal byte_addr : std_logic_vector(1 downto 0);
+signal sel : std_logic_vector(3 downto 0);
 begin
     addr <= "0000"&i_m_wb.addr(11 downto 2)  ;
+    byte_addr <= i_m_wb.addr(1 downto 0)  ;
+    sel <= i_m_wb.sel;
     data <= mem(to_integer(unsigned((addr)))) when i_m_wb.stb='1'  and i_m_wb.cyc='1' else (others => '0');
     process(i_clk,i_rst)
     begin
@@ -36,11 +40,23 @@ begin
         elsif rising_edge(i_clk) then
             if i_m_wb.we = '1' and i_m_wb.stb='1' and i_m_wb.cyc='1' then
                 if i_m_wb.sel = "0001" then
-                    mem(to_integer(unsigned(addr))) <= data(31 downto 8) & i_m_wb.data(7 downto 0);
+                    --mem(to_integer(unsigned(addr))) <= data(31 downto 8) & i_m_wb.data(7 downto 0);
+                    if byte_addr="00" then
+                        mem(to_integer(unsigned(addr))) <= data(31 downto 8) & i_m_wb.data(7 downto 0);
+                    elsif byte_addr="01" then
+                        mem(to_integer(unsigned(addr))) <= data(31 downto 16) & i_m_wb.data(7 downto 0) & data(7 downto 0) ;
+                    elsif byte_addr="10" then
+                        mem(to_integer(unsigned(addr))) <= data(31 downto 24) & i_m_wb.data(7 downto 0) & data(15 downto 0) ;
+                    elsif byte_addr="11" then
+                        mem(to_integer(unsigned(addr))) <= i_m_wb.data(7 downto 0) & data(23 downto 0) ;
+                    end if;
                 elsif i_m_wb.sel = "0011" then
                     mem(to_integer(unsigned(addr))) <= data(31 downto 16) & i_m_wb.data(15 downto 0);
-                elsif i_m_wb.sel = "0111" then
-                    mem(to_integer(unsigned(addr))) <= data(31 downto 24) & i_m_wb.data(23 downto 0);
+                    if byte_addr="01" then
+                        mem(to_integer(unsigned(addr))) <= data(31 downto 16) & i_m_wb.data(15 downto 0);
+                    elsif byte_addr="11" then
+                        mem(to_integer(unsigned(addr))) <= i_m_wb.data(15 downto 0) & data(15 downto 0);
+                    end if;
                 elsif i_m_wb.sel = "1111" then
                     mem(to_integer(unsigned(addr))) <= i_m_wb.data;
                 else
@@ -50,6 +66,29 @@ begin
         end if;
     end process;
 
-    o_m_wb.data <= mem(to_integer(unsigned((addr)))) when  i_m_wb.stb='1'  and i_m_wb.cyc='1';-- and rising_edge(i_clk);
+    process(addr,sel,byte_addr,data)
+    begin
+        if sel="1111" then
+            o_m_wb.data <=data;
+        elsif sel="0011" then
+            if byte_addr="01" then
+                o_m_wb.data <=x"0000" & data(15 downto 0);
+            elsif byte_addr="11" then
+                o_m_wb.data <=x"0000" & data(31 downto 16);
+            end if;
+        elsif sel="0001" then
+            if byte_addr="00" then
+                o_m_wb.data <=x"000000" & data(7 downto 0);
+            elsif byte_addr="01" then
+                o_m_wb.data <=x"000000" & data(15 downto 8);
+            elsif byte_addr="10" then
+                o_m_wb.data <=x"000000" & data(23 downto 16);
+            elsif byte_addr="11" then
+                o_m_wb.data <=x"000000" & data(31 downto 24);
+            end if;
+        end if;
+    end process;
+
+    --o_m_wb.data <= mem(to_integer(unsigned((addr)))) when  i_m_wb.stb='1'  and i_m_wb.cyc='1';-- and rising_edge(i_clk);
     o_m_wb.ack <= i_m_wb.stb and i_m_wb.cyc;-- when rising_edge(i_clk);
 end behave;
